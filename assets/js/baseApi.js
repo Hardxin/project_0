c441af57876d61a2538c4d35e6cf24b84ef11217
zangxin <1145357576@qq.com>
2020-09-14 15:34:15 +0800

    完成个人中心功能的开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,6 +1,6 @@
 $.ajaxPrefilter(function (options) {
     options.url = 'http://ajax.frontend.itheima.net' + options.url;
-    console.log(options.url);
+    // console.log(options.url);
 
     if (options.url.indexOf('/my/') !== -1) {
         options.headers = {
